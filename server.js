390e56b13ed02df612fbbd0677e55a7078376595
승희 <bangseunghee1108@gmail.com>
2023-12-03 22:40:44 +0900

    nodemon, MongoDB 설치

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 
 const PORT = 3000;
 
+const { MongoClient } = require('mongodb')
+
 app.listen(PORT, () => {
   console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
  });
