0b5353c2d9c965a3e29fc29e05244e4b6fc65a5b
승희 <bangseunghee1108@gmail.com>
2023-12-25 00:15:59 +0900

    포트 번호 수정

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 // const path = require("path"); 
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(express.static(__dirname + '/public'));
 // app.use(express.static(path.join(__dirname, 'public')));
